e289e7c43fba2e5755abe0335f776d51aaac2866
Mari Imaizumi <mariimaizumi5@gmail.com>
2026-01-05 13:31:34 +0900

    Fix key bindings

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/ima1zumi/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/ima1zumi/keymap.c
@@ -29,7 +29,7 @@ enum {
 
 // 2) Tap Dance の動作を定義（1回=kc1、2回=kc2）
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_C_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_C, KC_ESC),
+    [TD_D_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_D, KC_ESC),
     [TD_QUOT_DQUO] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_DQUO),
     [TD_COLN_SCLN] = ACTION_TAP_DANCE_DOUBLE(KC_COLN, KC_SCLN)
 };
@@ -38,14 +38,14 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_Q           , KC_W    , KC_E          , KC_R           , KC_T         ,                                         KC_Y , KC_U         , KC_I    , KC_O    , KC_P         ,
-    KC_A           , KC_S    , KC_D          , CTL_T(KC_F)    , KC_G         ,                                         KC_H , KC_J         , KC_K    , KC_L    , KC_MINS      ,
-    SFT_T(KC_Z)    , KC_X    , TD(TD_C_ESC)  , KC_V           , KC_B         ,                                         KC_N , KC_M         , KC_BTN1 , KC_BTN2 , LT(3,KC_TAB) ,
-    SFT_T(KC_COMM) , KC_LCTL , ALT_T(KC_ESC) , CMD_T(KC_LNG2) , LT(1,KC_SPC) , LT(2,KC_LNG1) ,       KC_BSPC , LT(2,KC_ENT) , XXXXXXX      , XXXXXXX , XXXXXXX , KC_DOT
+    KC_A           , KC_S    , TD(TD_D_ESC)  , CTL_T(KC_F)    , KC_G         ,                                         KC_H , KC_J         , KC_K    , KC_L    , KC_MINS      ,
+    SFT_T(KC_Z)    , KC_X    , KC_C          , KC_V           , KC_B         ,                                         KC_N , KC_M         , KC_BTN1 , KC_BTN2 , LT(3,KC_TAB) ,
+    SFT_T(KC_COMM) , KC_LCTL , ALT_T(KC_ESC) , CMD_T(KC_LNG1) , LT(1,KC_SPC) , LT(2,KC_LNG2) ,       KC_BSPC , LT(2,KC_ENT) , XXXXXXX      , XXXXXXX , XXXXXXX , KC_DOT
   ),
 
   [1] = LAYOUT_universal(
     KC_1           , KC_2    , KC_3          , KC_4           , KC_5         ,                                         KC_6 , KC_7         , KC_8    , KC_9    , KC_0         ,
-    XXXXXXX        , XXXXXXX , XXXXXXX       , XXXXXXX        , XXXXXXX      ,                                      XXXXXXX , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX      ,
+    XXXXXXX        , XXXXXXX , XXXXXXX       , XXXXXXX        , XXXXXXX      ,                                      KC_LEFT , KC_DOWN      , KC_UP   , KC_RGHT , XXXXXXX      ,
     KC_F1          , KC_F2   , KC_F3         , KC_F4          , KC_F5        ,                                        KC_F6 , KC_F7        , KC_F8   , KC_F9   , KC_F10       ,
     XXXXXXX        , XXXXXXX , XXXXXXX       , XXXXXXX        , _______      , _______       , _______ ,            _______ , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX
   ),
