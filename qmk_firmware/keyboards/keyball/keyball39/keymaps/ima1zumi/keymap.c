c55a2ab86e4d11e4b1ca0e2c984ce7165205f833
Mari Imaizumi <mariimaizumi5@gmail.com>
2026-01-14 00:33:39 +0900

    Update

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/ima1zumi/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/ima1zumi/keymap.c
@@ -54,7 +54,7 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_Q           , KC_W        , KC_E          , KC_R           , KC_T         ,                                         KC_Y , KC_U         , KC_I    , KC_O    , KC_P         ,
-    KC_A           , KC_S        , KC_D          , CTL_T(KC_F)    , KC_G         ,                                         KC_H , KC_J         , KC_K    , KC_L    , LT(3,KC_MINS),
+    CTL_T(KC_A)    , KC_S        , KC_D          , CTL_T(KC_F)    , KC_G         ,                                         KC_H , KC_J         , KC_K    , KC_L    , LT(3,KC_MINS),
     SFT_T(KC_Z)    , CTL_T(KC_X) , KC_C          , KC_V           , KC_B         ,                                         KC_N , KC_M         , KC_BTN1 , KC_BTN2 , KC_TAB ,
     SFT_T(KC_COMM) , KC_LCTL     , ALT_T(KC_ESC) , CMD_T(KC_LNG1) , LT(1,KC_SPC) , LT(2,KC_LNG2) ,       KC_BSPC , LT(2,KC_ENT) , XXXXXXX      , XXXXXXX , XXXXXXX , KC_DOT
   ),
